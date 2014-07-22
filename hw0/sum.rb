6e1592c247dec884b675795593281585aaf3a4c2
ztoolson <zach.toolson@gmail.com>
2014-07-22 10:19:35 -0600

    Start of homework0. finished sum functions

---
+++ hw0/sum.rb
@@ -0,0 +1,25 @@
+# Define a method sum which takes an array of integers as an
+# argument and return the sum of its elements.
+def sum(arr)
+  return 0 if arr.length == 0
+  arr.reduce(:+)
+end
+
+# Define a method max_2_sum which takes an array of integers as an
+# argument and returns the sum of the largers 2 elements.
+#
+# For an empty array it should return 0, for an array with just one
+# element, it should return that element
+def max_2_sum(arr)
+  return 0 if arr.length == 0
+  arr.sort.reverse[0 .. 1].reduce(:+)
+end
+
+# Define a method sum_to_n? which takes an array of integers and an 
+# additional integer, n, as arguments and return true if any two distinct
+# elements in the array sum to n. An empty array or single element array
+# should both return false.
+def sum_to_n?(arr, n)
+  return false if arr.length == 0 or arr.length == 1
+  arr.permutation(2).any? { |a, b| a + b == n }
+end
