3a62b390039d3338df6306655cdb60d08245b718
Zach Toolson <zach.toolson@gmail.com>
2015-03-18 13:11:17 -0600

    Update hw0 to use Arr#empty? method

--- hw0/sum.rb
+++ hw0/sum.rb
@@ -1,7 +1,7 @@
 # Define a method sum which takes an array of integers as an
 # argument and return the sum of its elements.
 def sum(arr)
-  return 0 if arr.length == 0
+  return 0 if arr.empty?
   arr.reduce(:+)
 end
 
@@ -11,8 +11,8 @@ end
 # For an empty array it should return 0, for an array with just one
 # element, it should return that element
 def max_2_sum(arr)
-  return 0 if arr.length == 0
-  arr.sort.reverse[0 .. 1].reduce(:+)
+  return 0 if arr.empty?
+  arr.sort.reverse[0..1].reduce(:+)
 end
 
 # Define a method sum_to_n? which takes an array of integers and an 
@@ -20,6 +20,6 @@ end
 # elements in the array sum to n. An empty array or single element array
 # should both return false.
 def sum_to_n?(arr, n)
-  return false if arr.length == 0 or arr.length == 1
+  return false if arr.empty? || arr.length == 1
   arr.permutation(2).any? { |a, b| a + b == n }
 end
