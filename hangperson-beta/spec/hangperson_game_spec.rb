db2132a0932a0abdb40491eb6bcdc3fc9177e060
Zach Toolson <zach.toolson@gmail.com>
2015-04-01 17:23:14 -0600

    working hangman game

--- hangperson-beta/spec/hangperson_game_spec.rb
+++ hangperson-beta/spec/hangperson_game_spec.rb
@@ -8,82 +8,100 @@ describe HangpersonGame do
       game.guess(letter)
     end
   end
-  describe 'new', :pending => true do
-    subject { HangpersonGame.new('glorp') }
+
+  describe 'new' do
+    subject { HangpersonGame.new('glorp') } # instantiate the Hangperson game with the work 'glorp'
     its(:word)    { should == 'glorp' }
     its(:guesses) { should == '' }
     its(:wrong_guesses) { should == '' }
   end
+
   describe 'guessing' do
     context 'correctly' do
       before :each do
         @game = HangpersonGame.new('garply')
         @valid = @game.guess('a')
       end
-      it 'changes correct guess list', :pending => true do
+
+      it 'changes correct guess list' do
         @game.guesses.should == 'a'
         @game.wrong_guesses.should == ''
       end
-      it 'returns true', :pending => true do
+
+      it 'returns true' do
         @valid.should_not be(false)
       end
     end
+
     context 'incorrectly' do
       before :each do
         @game = HangpersonGame.new('garply')
         @valid = @game.guess('z')
       end
-      it 'changes wrong guess list', :pending => true do
+
+      it 'changes wrong guess list' do
         @game.guesses.should == ''
         @game.wrong_guesses.should == 'z'
       end
-      it 'returns true', :pending => true do
+
+      it 'returns true' do
         @valid.should_not be(false)
       end
     end
+
     context 'same letter repeatedly' do
       before :each do
         @game = HangpersonGame.new('garply')
         guess_several_letters(@game, 'aq')
       end
-      it 'does not change correct guess list', :pending => true do
+
+      it 'does not change correct guess list' do
         @game.guess('a')
         @game.guesses.should == 'a'
       end
-      it 'does not change wrong guess list', :pending => true do
+
+      it 'does not change wrong guess list' do
         @game.guess('q')
         @game.wrong_guesses.should == 'q'
       end
-      it 'returns false', :pending => true do
+
+      it 'returns false' do
         @game.guess('a').should be(false)
         @game.guess('q').should be(false)
       end
     end
+
     context 'invalid' do
       before :each do
         @game = HangpersonGame.new('foobar')
       end
-      it 'throws an error when empty', :pending => true do
+
+      it 'throws an error when empty' do
         expect { @game.guess('') }.to raise_error(ArgumentError)
       end
-      it 'throws an error when not a letter', :pending => true do
+
+      it 'throws an error when not a letter' do
         expect { @game.guess('%') }.to raise_error(ArgumentError)
       end
-      it 'throws an error when nil', :pending => true do
+
+      it 'throws an error when nil' do
         expect { @game.guess(nil) }.to raise_error(ArgumentError)
       end
     end
   end
-  describe 'displayed word with guesses', :pending => true do
+
+  describe 'displayed word with guesses' do
     before :each do
       @game = HangpersonGame.new('banana')
     end
+
     # for a given set of guesses, what should the word look like?
     @test_cases = {
       'bn' =>  'b-n-n-',
       'def' => '------',
       'ban' => 'banana'
     }
+
     @test_cases.each_pair do |guesses, displayed|
       it "should be '#{displayed}' when guesses are '#{guesses}'" do
         guess_several_letters(@game, guesses)
@@ -91,25 +109,25 @@ describe HangpersonGame do
       end
     end
   end
+
   describe 'game status' do
     before :each do 
       @game = HangpersonGame.new('dog')
     end
-    it 'should be win when all letters guessed', :pending => true do
+
+    it 'should be win when all letters guessed' do
       guess_several_letters(@game, 'ogd')
       @game.check_win_or_lose.should == :win
     end
-    it 'should be lose after 7 incorrect guesses', :pending => true do
+
+    it 'should be lose after 7 incorrect guesses' do
       guess_several_letters(@game, 'tuvwxyz')
       @game.check_win_or_lose.should == :lose
     end
-    it 'should continue play if neither win nor lose', :pending => true do
+
+    it 'should continue play if neither win nor lose' do
       guess_several_letters(@game, 'do')
       @game.check_win_or_lose.should == :play
     end
   end
 end
-
-    
-    
-    
